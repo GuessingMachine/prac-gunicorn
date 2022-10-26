ee5b8974352903357acf68cc008cf3a776874cb9
GuessingMachine <tjd125gns@naver.com>
2022-10-26 17:40:10 +0900

    ver:3.6 쓰레기값

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+
 from flask import Flask
 
 app = Flask(__name__)
 @app.route('/')
 def hello_world():
     result = {"code" : 200, "message" : "Hello World!"}
-   	return result
+    return result
 
 if __name__ == '__main__':
     app.run()
