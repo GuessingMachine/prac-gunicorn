0c30e0a22d2d538880503ca321331619d00e2231
GuessingMachine <tjd125gns@naver.com>
2022-10-27 11:43:40 +0900

    ver:5.0 main.py 코드 수정

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 app = Flask(__name__)
 core = CORS(app, resources={r"/*" : {"origins" : "*"}})
-@app.route('/')
+@app.route('/hello')
 def hello_world():
     result = {"code" : 200, "message" : "Hello World!"}
     return result
