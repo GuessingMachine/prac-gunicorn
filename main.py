60fb67e026338478bb17b14cc001bc3d498c90ab
GuessingMachine <tjd125gns@naver.com>
2022-10-27 15:08:56 +0900

    ver:6.0 main.py에 mysql에 추가

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
+import pymysql
+host = "192.168.197.161"
+user = "root"
+db = "test"
+passwd = "qwer1234"
+port = 30000
 
 app = Flask(__name__)
 core = CORS(app, resources={r"/*" : {"origins" : "*"}})
 @app.route('/hello')
 def hello_world():
-    result = {"code" : 200, "message" : "Hello World!"}
-    return result
+    DB = pymysql.connect(host=host, user=user, db=db, password=passwd, port=port, charset='utf8')
+    curs = DB.cursor()
+    sql = "select * from student";
+    curs.execute(sql)
+    rows = curs.fetchall()
+    return rows[0]
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
