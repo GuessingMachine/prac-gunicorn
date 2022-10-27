988ac3f8978f4032bff0fb12fba2ceb64a38ccd0
GuessingMachine <tjd125gns@naver.com>
2022-10-27 16:10:30 +0900

    ver: 7.0 콘피그맵 사용 한글깨짐 수정4

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 from flask import Flask
 from flask_cors import CORS
 import pymysql
+import os
 
 app = Flask(__name__)
 core = CORS(app, resources={r"/*" : {"origins" : "*"}})
 @app.route('/hello')
 def hello_world():
-    host = "mysql-svc"
-    user = "root"
-    db = "test"
-    passwd = "qwer1234"
+    host = os.environ['DB_HOST']
+    user = os.environ['DB_USER']
+    passwd = os.environ['MYSQL_ROOT_PASSWORD']
+    db = os.environ['DB_NAME']
     DB = pymysql.connect(host=host, user=user, db=db, password=passwd, charset="utf8mb4")
     curs = DB.cursor()
     sql = "select * from student";
     curs.execute(sql)
     rows = curs.fetchall()
-    dic = {}
-    for i in rows:
-        dic[i[0]] = i[1]
-    return dic
+    result = {"code": 200, "message": rows}
+    return result
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
